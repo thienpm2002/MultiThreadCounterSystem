bc705f3ecaadb1b77ba701ef4ad335b2895f420a
Thien <ledinhthienpm@gmail.com>
2026-03-10 17:32:02 +0700

    create class Counter

---
+++ src/Counter.java
@@ -0,0 +1,15 @@
+public class Counter {
+    private int count;
+
+    public Counter() {
+        this.count = 0;
+    }
+
+    public void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
